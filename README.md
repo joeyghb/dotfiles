4f9b25b9d096f04b802279105313c09aabe17a85
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-11-10 10:17:18 +0800

    Init
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ README.md
@@ -0,0 +1,54 @@
+# dotfiles
+
+Linux or FreeBSD user config file
+
+Screenshot for bash
+
+![bash screenshot](screenshot/bash.png)
+
+Screenshot for tmux
+
+![bash screenshot](screenshot/tmux.png)
+
+## Usage
+
+```
+$ git clone https://github.com/appleboy/dotfiles.git && cd dotfiles && source bootstrap.sh
+```
+
+Install [spf13-vim](https://github.com/spf13/spf13-vim)
+
+```
+$ sh <(curl https://j.mp/spf13-vim3 -L)
+```
+
+## Tools
+
+* [x] [z.sh](https://github.com/rupa/z)
+* [x] [diff-so-fancy](https://github.com/stevemao/diff-so-fancy)
+* [x] [git-blame-someone-else](https://github.com/jayphelps/git-blame-someone-else)
+* [x] git-mv-with-history
+* [x] [git-recent](https://github.com/paulirish/git-recent)
+* [x] [git-open](https://github.com/paulirish/git-open)
+* [x] [git-standup](https://github.com/kamranahmedse/git-standup)
+* [x] [git-quick-stats](https://github.com/arzzen/git-quick-stats)
+* [x] [git-recall](https://github.com/Fakerr/git-recall)
+* [x] [wifi-password](https://github.com/rauchg/wifi-password) macOS only
+
+## Demo
+
+Download demo terminal from docker.
+
+```
+$ docker pull appleboy/docker-terminal
+```
+
+Login to docker terminal
+
+```
+$ docker run -ti appleboy/docker-terminal
+```
+
+# Author
+
+Bo-Yi Wu, Twitter: [@appleboy](http://twitter.com/appleboy "Twitter"), Blog: http://blog.wu-boy.com
---
+++ bootstrap.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+  rsync --exclude ".git/" --exclude "bootstrap.sh" \
+    --exclude "Makefile" --exclude "screenshot/" \
+    --exclude "README.md" --exclude "alpine/" \
+    --exclude "freebsd/" \
+    -avh --no-perms . ~;
+  source ~/.bashrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  doIt;
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
+fi;
+
+unset doIt;
+
+[ -d "${HOME}/bin" ] || mkdir ~/bin
+
+# download z.sh (https://github.com/rupa/z)
+[ -f "${HOME}/z.sh" ] && rm -rf ${HOME}/z.sh
+curl -L "https://raw.githubusercontent.com/rupa/z/master/z.sh" -o ~/z.sh
+chmod +x ~/z.sh
+
+# download git-blame-someone-else (https://github.com/jayphelps/git-blame-someone-else)
+curl -o ~/bin/git-blame-someone-else "https://raw.githubusercontent.com/jayphelps/git-blame-someone-else/master/git-blame-someone-else"
+chmod +x ~/bin/git-blame-someone-else
+
+# download diff-so-fancy (https://github.com/so-fancy/diff-so-fancy)
+[ -d "~/bin/lib" ] || mkdir -p ~/bin/lib
+curl -o ~/bin/lib/DiffHighlight.pm https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/lib/DiffHighlight.pm
+curl -o ~/bin/diff-so-fancy "https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/diff-so-fancy"
+chmod +x ~/bin/diff-so-fancy
+
+# download git-mv-with-history
+curl -o ~/bin/git-mv-with-history "https://gist.githubusercontent.com/emiller/6769886/raw/git-mv-with-history"
+chmod +x ~/bin/git-mv-with-history
+
+# See your latest local git branches, formatted real fancy
+# https://github.com/paulirish/git-recent
+curl -o ~/bin/git-recent "https://raw.githubusercontent.com/paulirish/git-recent/master/git-recent"
+chmod +x ~/bin/git-recent
+
+# Type `git open` to open the GitHub page or website for a repository in your browser.
+# https://github.com/paulirish/git-open
+curl -o ~/bin/git-open "https://raw.githubusercontent.com/paulirish/git-open/master/git-open"
+chmod +x ~/bin/git-open
+
+# Recall what you did on the last working day. Psst! or be nosy and find what someone else in your team did ;-)
+# https://github.com/kamranahmedse/git-standup
+curl -o ~/bin/git-standup "https://raw.githubusercontent.com/kamranahmedse/git-standup/master/git-standup"
+chmod +x ~/bin/git-standup
+
+# Git quick statistics is a simple and efficient way to access various statistics in git repository.
+# https://github.com/arzzen/git-quick-stats
+curl -o ~/bin/git-quick-stats "https://raw.githubusercontent.com/arzzen/git-quick-stats/master/git-quick-stats"
+chmod +x ~/bin/git-quick-stats
+
+# Simple and handy tool to easily recall what you've done
+# https://github.com/Fakerr/git-recall
+curl -o ~/bin/git-recall "https://raw.githubusercontent.com/Fakerr/git-recall/master/git-recall"
+chmod +x ~/bin/git-recall
+
+# Get the password of the wifi you're on (bash)
+# https://github.com/rauchg/wifi-password
+curl -L https://raw.github.com/rauchg/wifi-password/master/wifi-password.sh -o ~/bin/wifi-password
+chmod +x ~/bin/wifi-password
+
+# install tig command
+git clone  https://github.com/jonas/tig.git
+cd tig && make && make install
